f1b6bcd4000129d3d5d4d0a0ced27b4139ba9e58
coffeepenbit <coffeepenbit@gmail.com>
2022-02-24 21:19:11 -0500

    Remove printout of cwd

--- reset_gpu.ps1
+++ reset_gpu.ps1
@@ -9,7 +9,6 @@ $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Pri
 if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $false){
     [System.Windows.MessageBox]::Show('Must run {0} as admin' -f $PSCommandPath)
 } else {
-    Write-Output $PSScriptRoot
     Push-Location $PSScriptRoot
     # Reset to stock GPU settings to avoid crash after resetting power play table
     OverdriveNTool -c0"stock w/ fan curve"
